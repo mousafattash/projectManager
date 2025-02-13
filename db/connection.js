c3951a4f331ac332a87f8c0e08d5154335ac26d9
mousa <mousa@gmail.com>
2025-02-13 21:34:17 +0200

    change the deploy db cardenalties

--- db/connection.js
+++ db/connection.js
@@ -6,9 +6,9 @@ import { asyncHandler } from "../src/middleware/catchError.js";
 dotenv.config();
 // console.log("DB_PASS:", process.env.DB_PASS);
 
-export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: "postgres",
+export const sequelize = new Sequelize(freedb_projectsmanager, freedb_mousa, "pmfwG4G*XHasBG?", {
+  host: sql.freedb.tech,
+  dialect: "mysql",
 });
 
 export const connectDB = asyncHandler(async () => {
